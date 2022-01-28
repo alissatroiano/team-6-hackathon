9162f10bf9e665259b7cb327d334adec369d2f85
alissatroiano <alissatroianonyc@gmail.com>
2022-01-28 16:26:03 -0500

    Repair signup url

--- wEshare/posts/views.py
+++ wEshare/posts/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from allauth.account.forms import LoginForm
-
+from allauth.account.views import SignupView
 
 # Create your views here.
 def index(request):
@@ -8,5 +8,6 @@ def index(request):
     template = 'index.html'
     context ={
         'LoginForm': LoginForm,
+        'SignupView': SignupView,
     }
     return render(request, template, context)
