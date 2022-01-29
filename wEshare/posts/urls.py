c55148676d22cca04cfe31c78fa4950a7ee8a15b
alissatroiano <alissatroianonyc@gmail.com>
2022-01-29 08:08:59 -0500

    Resolve merge conflicts

--- wEshare/posts/urls.py
+++ wEshare/posts/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path, include
-from .views import index
+from .views import index, view_posts
 
 urlpatterns = [
     path('', index, name='index'),
     path('accounts/', include('allauth.urls')),
+    path('view_posts/', view_posts, name='view_posts'),
 ]
--- wEshare/posts/views.py
+++ wEshare/posts/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
+
 from allauth.account.forms import LoginForm
 from allauth.account.views import SignupView
+from django.contrib.auth.decorators import login_required
+
+from .models import Post
 
 # Create your views here.
 def index(request):
@@ -11,3 +15,10 @@ def index(request):
         'SignupView': SignupView,
     }
     return render(request, template, context)
+
+
+@login_required
+def view_posts(request):
+    """ A view to return the posts page """
+    posts = Post.objects.filter(user=request.user)
+    return render(request, 'posts.html', {'posts': posts})
