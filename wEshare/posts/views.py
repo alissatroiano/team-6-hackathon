3a0c3005ad02daae35f017c1838155d6e6a934c7
alissatroiano <alissatroianonyc@gmail.com>
2022-01-31 09:43:19 -0500

    Create methtod to expiree posts, update views, update tagline

--- wEshare/posts/views.py
+++ wEshare/posts/views.py
@@ -4,11 +4,14 @@ from allauth.account.forms import LoginForm
 from allauth.account.views import SignupView
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages  # added messages for editing success/warning
-
-from django.views.generic import UpdateView  # meant to use this as default django method in edit_post
-from django.forms.models import modelformset_factory
 from .models import Post
 from .forms import PostForm
+import datetime
+
+now = datetime.datetime.now()
+display_post = False
+if 9 < now.hour < 12:
+    display_post = True
 
 # Create your views here.
 def index(request):
@@ -53,21 +56,9 @@ def create_post(request):
 
     return render(request, template, context)
 
-# @login_required
-# def edit_post(request, post_id):
-#     """ Editing existing posts """
-#     post = get_object_or_404(Post, unique_id=post_id)
-#     PostFormSet = modelformset_factory(
-#         Post, fields=('description', 'content'),form=PostForm, extra=0)
-    
-#     if request.method == 'GET':
-#         formset = PostFormSet(queryset=Post.objects.filter(post=post))
-#         return render(request, 'edit_posts.html', {'formset': formset})
-#     else:
-#         formset = PostFormSet(request.POST, queryset=Post.objects.filter(post=post))
-#         if formset.is_valid():
-#             formset.save()
-#             return redirect('view_posts')
-#         else:
-#             return render(request, template, context)
- 
+
+def will_expire(self):
+    """ A view to set an expiration time on posts """
+    if self.date_published <= datetime.datetime.now():
+            self.delete()
+            return False
