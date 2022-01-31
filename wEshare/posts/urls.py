6894f0c3393f1275acade0a944c9cc3cc938bce6
Martina Grganic <totalnobzvz@yahoo.com>
2022-01-31 07:55:02 +0000

    All code is commented out in base.html, views.py, url.py and models.py - please check

--- wEshare/posts/urls.py
+++ wEshare/posts/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path, include
-from .views import index, view_posts, create_post
+from .views import index, view_posts, create_post # edit_post
 
 urlpatterns = [
     path('', index, name='index'),
     path('accounts/', include('allauth.urls')),
     path('view_posts/', view_posts, name='view_posts'),
     path('create_post/', create_post, name='create_post'),
+    # path('edit_post/<int:pk>/', edit_post.as_view(), name='edit_post') 
 ]
--- wEshare/posts/views.py
+++ wEshare/posts/views.py
@@ -2,7 +2,9 @@ from django.shortcuts import render, redirect, reverse
 from allauth.account.forms import LoginForm
 from allauth.account.views import SignupView
 from django.contrib.auth.decorators import login_required
+from django.contrib import messages  # added messages for editing success/warning
 
+from django.views.generic import UpdateView  # meant to use this as default django method in edit_post
 from django.forms.models import modelformset_factory
 from .models import Post
 from .forms import PostForm
@@ -51,3 +53,41 @@ def create_post(request):
     return render(request, template, context)
 
     return render(request, template, context)
+
+    # Dear Alissa, each video I watched that was uploaded within last year or less
+    # Has had a new html file with the edit/update post which this function would
+    # Redirect them, but we dont do it like that, I might be missing some logic behind
+    # the functions that you created and fair play for making those, but, after trying 
+    # this function still doesn't work, I would really have preffered that i figured
+    # it out but somehow I dont, I will wake up much before submission so maybe we
+    # could chat if I could be of any help, but I put this in comment since it would
+    # not open the page for me to test it, so this is why i will comment out anything
+    # related to 'edit_post' func, hope that any of this code helps but if not, feel
+    # free to arrange your own since the consistency is also relevant, its 7:40 so I
+    # will leave it in hope that maybe at least this could inspire your solution, you are
+    # amazing and please remember that, we have come a long way moctly because of you and
+    # your guidance, I appreciate you so much, sorry I did not manage this
+    
+    # @login_required
+    # def edit_post(UpdateView):
+    #     """ Editing existing posts """
+    #     posts_value = Post.content
+
+    #     if request.method == 'POST':
+    #         form = PostForm(request.POST)
+    #     try:
+    #         if form.is_valid():
+    #             form.save
+    #             post.user = request.user
+    #             messages.success(request, 'Your post was edited')
+    #             return redirect(reverse('edit_post'))
+        
+    #     except Exception as e:
+    #         messages.warning(request, 'Your post was not edited this time')
+    #     else:
+    #         form = PostForm()
+    #     context = {
+    #         'posts': post
+    #     }
+
+    #     return render(request, template, context)
