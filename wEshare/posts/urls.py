97119d1c9dad05bd5a82471ca324286ccba60198
alissatroiano <alissatroianonyc@gmail.com>
2022-01-31 06:54:37 -0500

    Touch up styles to make project more consistent

--- wEshare/posts/urls.py
+++ wEshare/posts/urls.py
@@ -1,10 +1,9 @@
 from django.urls import path, include
-from .views import index, view_posts, create_post, edit_post
+from .views import index, view_posts, create_post
 
 urlpatterns = [
     path('', index, name='index'),
     path('accounts/', include('allauth.urls')),
     path('view_posts/', view_posts, name='view_posts'),
     path('create_post/', create_post, name='create_post'),
-    path('edit_post/<str:post_id>/', edit_post, name='edit_post') 
 ]
